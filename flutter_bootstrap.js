504f1cb6d726dca5ecf911c75b18f6464b6208c5
Mohammad Asma Begum <mdasma1304@gmail.com>
2026-07-28 18:49:24 +0530

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2850134540" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2650815173" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
